0b0fac673886f794c25960f9ad3136981ad1a452
root <root@ip-172-31-39-136.us-west-2.compute.internal>
2015-09-16 01:53:49 +0000

    Updated the startwsgi file

--- saltstack/salt/resources/etc/gator/startuwsgi.sh
+++ saltstack/salt/resources/etc/gator/startuwsgi.sh
@@ -2,12 +2,12 @@
 
 
 count=0
-while [ ! -e "/var/gator/api/rest_handler.py" ]
+while [ ! -e "/var/gator/api/gator/rest_handler.py" ]
 do
         echo "Checking $checking"
         if [ $count == 10 ]
         then
-                echo "the file /var/gator/api/rest_handler.py does not exist"
+                echo "the file /var/gator/api/gator/rest_handler.py does not exist"
                 exit 1
         fi
 
