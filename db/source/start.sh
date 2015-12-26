a270416bf0103690b2b5201e66aba8695c5c2ee7
Austin Middleton <xuset1@gmail.com>
2015-12-25 19:44:35 -0600

    Added dynamo proxy and lambda server
    These two servers were added to the db container since the db
    container is only used locally and since those two servers are
    more closely related to the database than other parts of the
    system. Reference 06c6836c7@OrderApi for more info.
    
    track OrderApi#76

---
+++ db/source/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd /var/gator/db
+java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb -delayTransientStatuses -port 8041 &
+
+cd /var/gator/api/notify
+nodejs lambdaserver.js &
+
+cd /var/gator/api
+exec gunicorn --error-logfile - -b 0.0.0.0:8040 --reload gator.dynamoproxy:app
